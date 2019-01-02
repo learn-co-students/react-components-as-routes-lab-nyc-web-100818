25aecce46451b916514ddcee86d2e7c93ef83b6e
sg3957 <sg3957@nyu.edu>
2019-01-02 18:41:42 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const renderDirectors = () => {
+     return directors.map(d => (
+         <div>
+           {d.name}
+           <ul>
+             {d.movies.map(m => <li>{m}</li>)}
+           </ul>
+         </div>
+       )
+     )
+   }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
-}
+};
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        {
+          <h1>Movies Page</h1>
+
+
+
+
+        }
+      {  movies.map(m=>{
+          return <div>
+          <h3>{m.title}</h3>
+          <p>{m.time}</p>
+          <ul>{m.genres.map(g=>{
+            return <li>{g}</li>
+          })}</ul>
+          </div>
+        })}
     </div>
   );
 };
